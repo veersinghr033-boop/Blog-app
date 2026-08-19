0215198a422f5f4d8c41095ea52d9728474de027
Veer Singh <veersinghr033@gmail.com>
2026-08-19 17:29:11 +0530

    new\

--- apps/server/src/controllers/authControllers.ts
+++ apps/server/src/controllers/authControllers.ts
@@ -120,7 +120,7 @@ export const loginUser = async (
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
