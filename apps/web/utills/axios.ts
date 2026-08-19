43e4f85b4931a86dee177a893ad89db5bdd94aa0
Veer Singh <veersinghr033@gmail.com>
2026-08-19 17:15:27 +0530

    new\

--- apps/web/utills/axios.ts
+++ apps/web/utills/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blog-app-server-drab-chi.vercel.app/api",
+  baseURL: "https://blog-app-server-virid.vercel.app/api",
   withCredentials: true,
 });
 
