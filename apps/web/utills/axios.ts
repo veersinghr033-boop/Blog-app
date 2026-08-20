c925ba75304c33cd3769c4354ce67f4749c6e783
Veer Singh <veersinghr033@gmail.com>
2026-08-20 14:48:18 +0530

    new

--- apps/web/utills/axios.ts
+++ apps/web/utills/axios.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://blog-app-server-virid.vercel.app/api",
 
+  
   // baseURL: "http://localhost:5050/api",
   withCredentials: true,
 });
